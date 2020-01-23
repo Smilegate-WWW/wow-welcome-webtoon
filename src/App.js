dcdc8a0f96acd39ffffc455740d083c06f0d6e7f
hyomin <gyals6349@gmail.com>
2020-01-23 20:45:12 +0900

    Input Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
+import Logo from './Components/Logo';
 import './App.css';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles=theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit*3,
+    overflowX:"auto"
+  }
+})
+
 
 function App() {
   return (
     <div>
-      
+      <Logo></Logo>
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles) (App);
---
+++ src/Components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Logo extends React.Component{
+    render(){
+        return(
+            <img src="/images/logo.png" alt="logo"/>
+        )
+    }
+}
+
+export default Logo;
