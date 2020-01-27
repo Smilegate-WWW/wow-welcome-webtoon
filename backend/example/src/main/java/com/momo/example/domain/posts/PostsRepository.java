e8acc48e7dc8ec88977ccbe5923889d630f51a8f
momo <mjh79017901@gmail.com>
2020-01-27 10:51:57 +0900

    #7 예제 프로젝트 추가
    example 프로젝트 Application.Java 실행후 localhost:8080에서 내용 볼 수 있음
    
    example 프로젝트 test 오류 나는데 이건 나중에 해결

---
+++ backend/example/src/main/java/com/momo/example/domain/posts/PostsRepository.java
@@ -0,0 +1,13 @@
+package com.momo.example.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.stream.Stream;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p " +
+            "FROM Posts p " +
+            "ORDER BY p.id DESC")
+    Stream<Posts> findAllDesc();
+}
---
+++ backend/example/src/main/java/com/momo/example/web/WebController.java
@@ -0,0 +1,20 @@
+package com.momo.example.web;
+
+import com.momo.example.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+    private PostsService postsService;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "main";
+    }
+}
