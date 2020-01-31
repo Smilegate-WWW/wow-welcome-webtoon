cfd9111c95c7ba4dbbef506a923465b3cf45a9bd
Jyw526 <55542627+Jyw526@users.noreply.github.com>
2020-01-31 09:19:41 +0900

    01/31

---
+++ backend/upload_server/src/main/java/com/www/upload/UploadServerApplication.java
@@ -0,0 +1,13 @@
+package com.www.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadServerApplication.class, args);
+	}
+
+}
---
+++ backend/upload_server/src/test/java/com/www/upload/UploadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
