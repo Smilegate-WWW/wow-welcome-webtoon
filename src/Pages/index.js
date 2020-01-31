3dcd406305e4bbe48dd66161f37de64e9a942a76
hyomin <gyals6349@gmail.com>
2020-01-31 14:38:57 +0900

    ss

--- src/Pages/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Home} from './Home';
-export {default as CommentPage} from './CommentPage';
-export {default as LoginPage} from './LoginPage';
-export {default as MyPage} from './MyPage';
-export {default as SignupPage} from './SignupPage';
