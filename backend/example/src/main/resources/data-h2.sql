e5e78c7300d0892e4b38b15fe1e4fa9841aff13d
momo <mjh79017901@gmail.com>
2020-02-09 00:53:11 +0900

    댓글 저장, post 요청시 500, 400 에러 해결
    fk 컬럼 뺀 테스트 용도로 만든 db의 테이블에서 pk에 auto_inc 설정 안해서
    hibernate db연동시 에러가 발생한 듯. auto_inc 설정하니 post시 500error 안뜸
    
    그 대신 400 bad request 떴었는데 내용을 보낼 때 body의 json 내용이 잘 못됨.
    var data = {
                content: $('#content').val(),
            };
    ,를 붙여서 보냈어서 json 파싱을 잘 못 해가지고 에러가 떴던 것 같다.
    postman에서도 ,붙여서 보낼 시 동일하게 400error 발생해서 , 떼고 보내니 정상 작동
    
    example 프로젝트 제거

--- backend/example/src/main/resources/data-h2.sql
+++
@@ -1,3 +0,0 @@
-insert into posts (title, author, content, created_date, modified_date) values ('테스트1', 'test1@gmail.com', '테스트1의 본문', now(), now());
-insert into posts (title, author, content, created_date, modified_date) values ('테스트2', 'test2@gmail.com', '테스트2의 본문', now(), now());
-insert into posts (title, author, content, created_date, modified_date) values ('테스트3', 'test3@gmail.com', '테스트3의 본문', now(), now());
