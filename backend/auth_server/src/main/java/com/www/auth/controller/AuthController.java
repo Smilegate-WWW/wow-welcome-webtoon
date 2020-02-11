faf21f8c32f7d109cd615f8b24ba38b561e8dfce
ji-water <bjisoo72@gmail.com>
2020-02-11 20:51:41 +0900

    [#10] access token 재발급 및 로그아웃 API 추가
    * API 추가
    * reissue access token API [POST /users/{idx}/token]
    * logout API [DELETE /users/{idx}/token]
    
    * 토큰 재발급 요청시 access token 유효시간 30초 이내일 경우에만 재발급
    * access token 유효시간 1시간
    * refresh token 유효시간 24시간

--- backend/auth_server/src/main/java/com/www/auth/controller/AuthController.java
+++ backend/auth_server/src/main/java/com/www/auth/controller/AuthController.java
@@ -2,9 +2,11 @@ package com.www.auth.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,6 +19,7 @@ import com.www.auth.dto.Tokens;
 import com.www.auth.dto.UserDto;
 import com.www.auth.dto.UserLoginDto;
 import com.www.auth.dto.UserRegisterDto;
+import com.www.auth.service.JwtTokenProvider;
 import com.www.auth.service.UserService;
 
 import lombok.AllArgsConstructor;
@@ -33,9 +36,11 @@ import lombok.AllArgsConstructor;
 public class AuthController {
 
 	private UserService userService;
+	private JwtTokenProvider jwtTokenProvider;
 
 	/**
 	 * POST(/users) 회원가입 요청
+	 * 
 	 * @param user
 	 * @return
 	 */
@@ -45,11 +50,11 @@ public class AuthController {
 		Response<UserRegisterDto> result = new Response<UserRegisterDto>();
 		result.setCode(userService.register(user));
 		switch (result.getCode()) {
-		case 0://회원가입 성공
+		case 0:// 회원가입 성공
 			result.setMsg("insert complete");
 			result.setData(user);
 			break;
-		case 1://회원가입 실패
+		case 1:// 회원가입 실패
 			result.setMsg("insert fail");
 			break;
 		}
@@ -58,34 +63,104 @@ public class AuthController {
 
 	/**
 	 * POST(/users/token) LOGIN
+	 * 
 	 * @param userlogindto (id/pw)
 	 * @return auth header: access token, body : refresh token
 	 */
 	@PostMapping("/token")
-	public Response<String> Login(HttpServletResponse response,@RequestBody UserLoginDto userlogin) {
+	public Response<String> Login(HttpServletResponse response, @RequestBody UserLoginDto userlogin) {
 		Response<String> result = new Response<String>();
-		Tokens tokens=userService.login(userlogin);
-		if(tokens.getAccessToken()!=null) { //로그인 성공
+		Tokens tokens = userService.login(userlogin);
+		if (tokens.getAccessToken() != null) { // 로그인 성공
 			result.setCode(0);
 			result.setMsg("login complete");
 			result.setData(tokens.getRefreshToken());
-			response.addHeader(HttpHeaders.AUTHORIZATION, "bearer "+tokens.getAccessToken());
-		}
-		else { //로그인 실패
+			response.addHeader(HttpHeaders.AUTHORIZATION, "bearer " + tokens.getAccessToken());
+		} else { // 로그인 실패
 			result.setCode(2);
 			result.setMsg("auth fail: 'not exist id' or 'wrong pw'");
 		}
 		return result;
 	}
 
-	@DeleteMapping("/token")
-	public Response<String> Logout(@RequestHeader("Authentication") String AccessToken, @RequestBody String RefreshToken ){
+	/**
+	 * DELETE(/users/token) 로그아웃
+	 * @param AccessToken (auth header)
+	 * @param RefreshToken (request body)
+	 * @return expire tokens
+	 */
+	@DeleteMapping("/{idx}/token")
+	public Response<String> Logout(@RequestHeader("Authorization") String AccessToken,
+			@RequestBody String data,@PathVariable("idx") int useridx) {
+		
 		Response<String> result = new Response<String>();
-		//access token bearer split
-		//access token 유효검사
-		//refresh token 있는지 체크
-		//redis refresh token 버리기
+		// access token bearer split
+		AccessToken = AccessToken.substring(7);
+		JSONObject body = new JSONObject(data);
+		String RefreshToken = body.getString("RefreshToken");
+		System.out.println("====Idx:"+useridx+"=====");
+		System.out.println("AccessToken:"+AccessToken);
+		System.out.println("RefreshToken:"+RefreshToken);
+
+		int r=jwtTokenProvider.checkRefreshToken(AccessToken, RefreshToken,useridx);
+		switch(r){
+			case 41: //이미 로그아웃
+				result.setCode(41);
+				result.setMsg("logout");
+				break;
+			case 40: //refresh token 존재
+				jwtTokenProvider.expireToken(jwtTokenProvider.getUserName(AccessToken));
+				result.setCode(41);
+				result.setMsg("logout");
+				break;
+			case 42:
+				result.setCode(42);
+				result.setMsg("access denied : maybe captured or faked token");
+				break;
+		}
+
 		return result;
+	}
+	
+	/**
+	 * POST (/users/{user_idx}/token) access token 재발급 
+	 * @param AccessToken
+	 * @param data
+	 * @param useridx
+	 * @param response
+	 * @return response header에 access token 발급
+	 */
+	@PostMapping("/{idx}/token")
+	public Response<String> ReissueToken(@RequestHeader("Authorization") String AccessToken,
+			@RequestBody String data,@PathVariable("idx") int useridx,HttpServletResponse response){
+		Response<String> result = new Response<String>();
+		// access token bearer split
+		AccessToken = AccessToken.substring(7);
+		JSONObject body = new JSONObject(data);
+		String RefreshToken = body.getString("RefreshToken");
+		System.out.println("====Idx:"+useridx+"=====");
+		System.out.println("AccessToken:"+AccessToken);
+		System.out.println("RefreshToken:"+RefreshToken);
+		
+		int r=jwtTokenProvider.checkRefreshToken(AccessToken, RefreshToken, useridx);
+		switch(r) {
+		case 40:
+			String newAT=jwtTokenProvider.createAccessToken(useridx, jwtTokenProvider.getUserName(AccessToken));
+			response.addHeader(HttpHeaders.AUTHORIZATION, "bearer " + newAT);
+			result.setCode(40);
+			result.setMsg("reissue tokens");
+			break;
+		case 41:
+			result.setCode(41);
+			result.setMsg("access denied: logout, you need to re-login");
+			break;
+		case 42:
+			result.setCode(42);
+			result.setMsg("access denied: incorrect access");
+			break;
+		}
+		return result;
+		
 	}
 
 }
--- backend/auth_server/src/main/java/com/www/auth/service/JwtTokenProvider.java
+++ backend/auth_server/src/main/java/com/www/auth/service/JwtTokenProvider.java
@@ -12,94 +12,118 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
+import io.jsonwebtoken.*;
 
 @Service
-public class JwtTokenProvider{
-	
-    private String secretKey="secretkeyhelloimjwtsecretkeysecretkeyhelloimkeyjwtplz";
-	
+public class JwtTokenProvider {
+
+	private String secretKey = "secretkeyhelloimjwtsecretkeysecretkeyhelloimkeyjwtplz";
+
 	private SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 	private byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
-	private Key KEY = new SecretKeySpec(apiKeySecretBytes,signatureAlgorithm.getJcaName());	
-	private long tokenValidMilisecond = 1000L * 60 * 60; // 1시간만 토큰 유효
-    
+	private Key KEY = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+	private long tokenValidMilisecond = 1000L * 60 * 60; // 유효 1시간(1000*60*60)
+
 	@Autowired
-	RedisTemplate<String,Object> redisTemplate;
-	
-    // access jwt 토큰 생성
+	RedisTemplate<String, Object> redisTemplate;
+
+	// access jwt 토큰 생성
 	public String createAccessToken(int user_idx, String user_name) {
-		//payload
-        Claims claims = Jwts.claims();
-        claims.put("user_idx", user_idx);
-        claims.put("user_name",user_name);
-        
-        Date now = new Date();
-        return Jwts.builder()
-        		.setHeaderParam("typ","jwt")
-                .setClaims(claims) 
-                .setIssuedAt(now) 
-                .setExpiration(new Date(now.getTime() + tokenValidMilisecond)) 
-                .signWith(KEY,signatureAlgorithm) 
-                .compact();
-    }
-	//refresh token 
-	public String createRefreshToken(String user_name) {
-		System.out.println("redis template:"+redisTemplate);
-		
+		// payload
+		Claims claims = Jwts.claims();
+		claims.put("user_idx", user_idx);
+		claims.put("user_name", user_name);
+
 		Date now = new Date();
-		String refreshToken = Jwts.builder()
-				.setHeaderParam("typ","jwt")
-				.setIssuedAt(now) 
-                .setExpiration(new Date(now.getTime() + tokenValidMilisecond*24)) //일단 하루만 유효 
-                .signWith(KEY,signatureAlgorithm) 
-                .compact();
-		//redis set (유효시간 설정 필요)
+		return Jwts.builder().setHeaderParam("typ", "jwt").setClaims(claims).setIssuedAt(now)
+				.setExpiration(new Date(now.getTime() + tokenValidMilisecond)).signWith(KEY, signatureAlgorithm)
+				.compact();
+	}
+
+	// refresh token
+	public String createRefreshToken(String user_name) {
+		System.out.println("redis template:" + redisTemplate);
+
+		Date now = new Date();
+		String refreshToken = Jwts.builder().setHeaderParam("typ", "jwt").setIssuedAt(now)
+				.setExpiration(new Date(now.getTime() + tokenValidMilisecond * 24)) // 일단 하루만 유효
+				.signWith(KEY, signatureAlgorithm).compact();
+		// redis set
 		ValueOperations<String, Object> vop = redisTemplate.opsForValue();
 		vop.set(user_name, refreshToken);
-		redisTemplate.expire(user_name, 100000, TimeUnit.MILLISECONDS);
-		
+		// 24시간 이후 refresh token 자동 삭제
+		redisTemplate.expire(user_name, tokenValidMilisecond * 24, TimeUnit.MILLISECONDS); 
+
 		return refreshToken;
 	}
-    
-    //jwt token에서 idx추출
-    public int getUserIdx(String token) {
-    	return (int) Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("user_idx");
-    }
-    
-    //jwt token에서 name 추출
-    public String getUserName(String token) {
-    	return (String) Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("user_name");
-    }
- 
-    // Jwt 토큰의 유효성 확인
-    public boolean validateToken(String jwtToken) {
-        try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken).getBody();
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-    
-    //refresh token 검증
-    public boolean checkRefreshToken(String accessT, String refreshT) {
-    	String user_name = getUserName(accessT);
-    	try {
-    		ValueOperations<String, Object> vop = redisTemplate.opsForValue();
-    		String redis_T = (String) vop.get(user_name);
-    		System.out.println("redis refresh token: "+redis_T);
-    		
-    		if(redis_T.equals(refreshT))
-    			return true;
-    		else
-    			return false;
-    	}catch(Exception e) {
-    		return false;
-    	}
-    	
-    }
+
+	// jwt token에서 idx추출
+	public int getUserIdx(String token) {
+		try {
+			return (int) Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("user_idx");
+		} catch (ExpiredJwtException e) {
+			return (int) e.getClaims().get("user_idx");
+		}
+	}
+
+	// jwt token에서 name 추출
+	public String getUserName(String token) {
+		try {
+			return (String) Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("user_name");
+		} catch (ExpiredJwtException e) {
+			return (String) e.getClaims().get("user_name");
+		}
+	}
+
+	// Jwt 토큰의 유효성 확인
+	public int validateToken(String jwtToken) {
+		System.out.println("validateToken parameter:"+jwtToken);
+		try {
+			Date now = new Date();
+			long time = (Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken).getBody().getExpiration().getTime()-now.getTime());
+			System.out.println("남은 시간! : "+time);
+			if(time < 1000L*30) {
+				return 1; //30초 남았을 경우에도 expired됐다고 판단
+			}
+			return 0; // 유효
+		} catch (ExpiredJwtException e) {
+			return 1; // expired
+		} catch (Exception e) {
+			System.out.println("validateToken 에러!!!:"+e);
+			return 2; // 구조적인 문제 있음
+		}
+	}
+
+	// refresh token 검증
+	public int checkRefreshToken(String accessT, String refreshT, int idx) {
+		// 토큰 유효한지 검사 (access token은 재발급 필요한 상태여야함!)
+		if (validateToken(accessT) == 1 && validateToken(refreshT) < 2 && getUserIdx(accessT) == idx) {
+			String user_name = getUserName(accessT);
+			try {
+				ValueOperations<String, Object> vop = redisTemplate.opsForValue();
+				String redis_T = (String) vop.get(user_name);
+				System.out.println("redis refresh token: " + redis_T);
+
+				if (redis_T == null)
+					return 41; // 재 로그인 필요 (로그아웃)
+				if (redis_T.equals(refreshT))
+					return 40; // access token 재발급
+				else
+					return 42; // 받은 refresh token과 저장된 refresh token 같지않음
+			} catch (Exception e) {
+				System.out.println(e);
+				return 42; // error 존재
+			}
+		}
+		return 42; // 유효하지않은 토큰들
+	}
+
+	// redis refresh token 삭제
+	public void expireToken(String name) {
+		try {
+			redisTemplate.delete(name);
+		} catch (Exception e) {
+			System.out.println("삭제 에러발생!!!!" + e);
+		}
+	}
 }
