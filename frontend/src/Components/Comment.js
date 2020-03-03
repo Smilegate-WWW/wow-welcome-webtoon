a9eee2e8f6600536c55b93b9f2940fe7770c8a56
ji-water <bjisoo72@gmail.com>
2020-03-04 04:34:32 +0900

    [#35] comment component 수정
    * like/dislike 버튼 눌렀을 때 결과가 바로 렌더링 되도록 state 수정했습니다 ~~~!!
    
    [!!] component쪽 error 44 처리 안했습니다

--- frontend/src/Components/Comment.js
+++ frontend/src/Components/Comment.js
@@ -1,89 +1,91 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 
-var Episode = require("../Pages/Episode")
-
 class Comment extends Component {
+    state={goodNum:this.props.goodNum,badNum:this.props.badNum};
+
+    handleGood = ({target:{goodNum}}) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("AUTHORIZATION", localStorage.getItem("AUTHORIZATION"));
+
+        var raw = "";
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("/comments/"+this.props.cmt_idx+"/like", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result)
+                if(result.code==0){
+                    this.setState({goodNum:result.data.cnt});
+                }
+                else if(result.code==21){
+                    alert("[ERROR 21] 잘못된 접근입니다, 관리자에게 문의하세요.")
+                }
+                else if(result.code==24){
+                    alert("자신이 단 댓글을 추천할 수 없습니다.")
+                }
+                else if(result.code==44){
+                    //만료된 토큰 처리 필요
+                }
+                else if(result.code==42){
+                    alert("[ERROR 42] 잘못된 접근입니다, 관리자에게 문의하세요.")
+                }
+                else{
+                    alert("잘못된 접근입니다, 관리자에게 문의하세요.")
+                }
+            })
+            .catch(error => console.log('error', error));
+    }
+
+    handleBad = ({target:{badNum}}) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("AUTHORIZATION", localStorage.getItem("AUTHORIZATION"));
+
+        var raw = "";
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("/comments/"+this.props.cmt_idx+"/dislike", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result)
+                if(result.code==0){
+                    this.setState({badNum:result.data.cnt})
+                }
+                else if(result.code==21){
+                    alert("[ERROR 21] 잘못된 접근입니다, 관리자에게 문의하세요.")
+                }
+                else if(result.code==25){
+                    alert("자신이 단 댓글을 비추천할 수 없습니다.")
+                }
+                else if(result.code==44){
+                    //만료된 토큰 처리 필요
+                }
+                else if(result.code==42){
+                    alert("[ERROR 42] 잘못된 접근입니다, 관리자에게 문의하세요.")
+                }
+                else{
+                    alert("잘못된 접근입니다, 관리자에게 문의하세요.")
+                }
+            })
+            .catch(error => console.log('error', error));
+    }
+
     render() {
-
-        const handleGood = () => {
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-            myHeaders.append("AUTHORIZATION", localStorage.getItem("AUTHORIZATION"));
-
-            var raw = "";
-
-            var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-            };
-
-            fetch("/comments/" + this.props.cmt_idx + "/like", requestOptions)
-                .then(response => response.json())
-                .then(result => {
-                    console.log(result)
-                    if (result.code == 0) { }
-                    else if (result.code == 21) {
-                        alert("[ERROR 21] 잘못된 접근입니다, 관리자에게 문의하세요.")
-                    }
-                    else if (result.code == 24) {
-                        alert("자신이 단 댓글을 추천할 수 없습니다.")
-                    }
-                    else if (result.code == 44) {
-                        //만료된 토큰 처리 필요
-                    }
-                    else if (result.code == 42) {
-                        alert("[ERROR 42] 잘못된 접근입니다, 관리자에게 문의하세요.")
-                    }
-                    else {
-                        alert("잘못된 접근입니다, 관리자에게 문의하세요.")
-                    }
-                    Episode.commentLoading()
-                })
-                .catch(error => console.log('error', error));
-        }
-
-        const handleBad = () => {
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-            myHeaders.append("AUTHORIZATION", localStorage.getItem("AUTHORIZATION"));
-
-            var raw = "";
-
-            var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-            };
-
-            fetch("/comments/" + this.props.cmt_idx + "/dislike", requestOptions)
-                .then(response => response.json())
-                .then(result => {
-                    console.log(result)
-                    if (result.code == 0) { }
-                    else if (result.code == 21) {
-                        alert("[ERROR 21] 잘못된 접근입니다, 관리자에게 문의하세요.")
-                    }
-                    else if (result.code == 25) {
-                        alert("자신이 단 댓글을 비추천할 수 없습니다.")
-                    }
-                    else if (result.code == 44) {
-                        //만료된 토큰 처리 필요
-                    }
-                    else if (result.code == 42) {
-                        alert("[ERROR 42] 잘못된 접근입니다, 관리자에게 문의하세요.")
-                    }
-                    else {
-                        alert("잘못된 접근입니다, 관리자에게 문의하세요.")
-                    }
-                    Episode.commentLoading()
-                })
-                .catch(error => console.log('error', error));
-        }
-
+        const {goodNum,badNum} = this.state;
 
         return (
             <div>
@@ -94,14 +96,16 @@ class Comment extends Component {
                     <div style={{ marginLeft: 580, display: "flex" }}>
                         <Button variant="contained" onClick={handleGood} style={{ marginRight: 10, width: 50 }}>
                             <img src="/Icon/commentGood.png" alt="icon" />
-                            &ensp;{this.props.goodNum}
+                            &ensp;{goodNum}
                         </Button>
                         <Button variant="contained" onClick={handleBad} style={{ marginRight: 5, width: 50 }}>
                             <img src="/Icon/commentBad.png" alt="icon" />
-                            &ensp;{this.props.badNum}
+                            &ensp;{badNum}
                         </Button>
                     </div>
                     <div style={{ display: "none" }}>{this.props.cmt_idx}</div>
+                    <div style={{display:"none"}}>{this.props.goodNum}</div>
+                    <div style={{display:"none"}}>{this.props.badNum}</div>
                 </div>
             </div>
         )
